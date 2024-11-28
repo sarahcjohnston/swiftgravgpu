405953d65238f8602b4fb8e394e42690f25410e5
wjr21 <wjr21@sussex.ac.uk>
2024-11-28 15:08:09 +0000

    Formatting without removing everything...

--- grav_pp_offload.cu
+++ grav_pp_offload.cu
@@ -11,7 +11,57 @@
 #include <unistd.h>
 
 /* Local Cuda includes */
-#include "cuda_streams.h"
+#include "src/cuda_streams.h"
+#include "src/gpu_params.h"
+
+extern "C" void gpu_device_props(struct gpu_info *gpu_info) {
+
+  /* Set the device ID */
+  cudaGetDevice(&gpu_info->device_id);
+
+  /* Get the device properties */
+  cudaDeviceProp deviceProp;
+  cudaGetDeviceProperties(&deviceProp, gpu_info->device_id);
+
+  /* Set the number of streaming multiprocessors */
+  gpu_info->nr_sm = deviceProp.multiProcessorCount;
+
+  /* Set the maximum number of threads per SM */
+  gpu_info->max_threads_per_sm = deviceProp.maxThreadsPerMultiProcessor;
+
+  /* Set the maximum number of threads per block */
+  gpu_info->max_threads_per_block = deviceProp.maxThreadsPerBlock;
+
+  /* Set the maximum number of blocks per SM */
+  gpu_info->max_blocks_per_sm = deviceProp.maxBlocksPerMultiProcessor;
+
+  /* Set the maximum amount of shared memory per SM */
+  gpu_info->max_shared_memory_per_sm = deviceProp.sharedMemPerMultiprocessor;
+
+  /* Set the maximum amount of shared memory per block */
+  gpu_info->max_shared_memory_per_block = deviceProp.sharedMemPerBlock;
+
+  /* Set the maximum number of registers per block */
+  gpu_info->max_registers_per_block = deviceProp.regsPerBlock;
+
+  /* Set the warp size */
+  gpu_info->warp_size = deviceProp.warpSize;
+
+  /* Set the maximum number of threads per block dimension */
+  gpu_info->max_threads_per_block_dimension = deviceProp.maxThreadsDim[0];
+
+  /* Set the maximum grid size */
+  gpu_info->max_grid_size = deviceProp.maxGridSize[0];
+
+  /* Set the maximum number of threads per block dimension x */
+  gpu_info->max_threads_per_block_dimension_x = deviceProp.maxThreadsDim[0];
+
+  /* Set the maximum number of threads per block dimension y */
+  gpu_info->max_threads_per_block_dimension_y = deviceProp.maxThreadsDim[1];
+
+  /* Set the maximum number of threads per block dimension z */
+  gpu_info->max_threads_per_block_dimension_z = deviceProp.maxThreadsDim[2];
+}
 
 // PP ALL INTERACTIONS
 __global__ void pair_grav_pp(
@@ -155,9 +205,8 @@ extern "C" void pp_offload(
     int cj_active, const int symmetric, const int allow_mpole,
     const struct multipole *restrict multi_i,
     const struct multipole *restrict multi_j, float *epsilon,
-    const int *allow_multipole_j, const int *allow_multipole_i) {
-
-  // cudaDeviceSynchronize();
+    const int *allow_multipole_j, const int *allow_multipole_i,
+    struct gpu_info *gpu_info) {
 
   float a_x_i_new[*gcount_i];
   float a_y_i_new[*gcount_i];
@@ -195,93 +244,100 @@ extern "C" void pp_offload(
   float *d_CoM_i;
   float *d_CoM_j;
 
+  /* Get a stream to use (for testing these run 0-3 inclusive), we'll
+   * randomly select one for now. */
+  int rand_stream = rand() % gpu_info->nr_streams;
+  cudaStream_t stream = streams->streams[rand_stream];
+
   // cudaDeviceSynchronize();
 
   // cudaMalloc(&h_multi_j, 13*sizeof(float));
   // cudaMemcpyAsync(h_multi_j, multi_j, 13*sizeof(float),
   // cudaMemcpyHostToDevice);
   multipole *d_multi_j;
-  cudaMalloc(&d_multi_j, sizeof(multipole));
-  cudaMemcpyAsync(d_multi_j, multi_j, sizeof(multipole),
-                  cudaMemcpyHostToDevice);
+  cudaMallocAsync(&d_multi_j, sizeof(multipole), stream);
+  cudaMemcpyAsync(d_multi_j, multi_j, sizeof(multipole), cudaMemcpyHostToDevice,
+                  stream);
   multipole *d_multi_i;
-  cudaMalloc(&d_multi_i, sizeof(multipole));
-  cudaMemcpyAsync(d_multi_i, multi_i, sizeof(multipole),
-                  cudaMemcpyHostToDevice);
+  cudaMallocAsync(&d_multi_i, sizeof(multipole), stream);
+  cudaMemcpyAsync(d_multi_i, multi_i, sizeof(multipole), cudaMemcpyHostToDevice,
+                  stream);
 
   // allocate memory on device
-  cudaMalloc(&d_h_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_h_j, *gcount_padded_j * sizeof(float));
-  cudaMalloc(&d_mass_i, *gcount_padded_i * sizeof(float));
-  cudaMalloc(&d_mass_j, *gcount_padded_j * sizeof(float));
-  cudaMalloc(&d_x_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_x_j, *gcount_padded_j * sizeof(float));
-  cudaMalloc(&d_y_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_y_j, *gcount_padded_j * sizeof(float));
-  cudaMalloc(&d_z_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_z_j, *gcount_padded_j * sizeof(float));
-  cudaMalloc(&d_a_x_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_a_y_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_a_z_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_a_x_j, *gcount_j * sizeof(float));
-  cudaMalloc(&d_a_y_j, *gcount_j * sizeof(float));
-  cudaMalloc(&d_a_z_j, *gcount_j * sizeof(float));
-  cudaMalloc(&d_pot_i, *gcount_i * sizeof(float));
-  cudaMalloc(&d_pot_j, *gcount_j * sizeof(float));
-  cudaMalloc(&d_active_i, *gcount_i * sizeof(int));
-  cudaMalloc(&d_mpole_i, *gcount_i * sizeof(int));
-  cudaMalloc(&d_active_j, *gcount_j * sizeof(int));
-  cudaMalloc(&d_mpole_j, *gcount_j * sizeof(int));
-  cudaMalloc(&d_CoM_i, 3 * sizeof(float));
-  cudaMalloc(&d_CoM_j, 3 * sizeof(float));
+  cudaMallocAsync(&d_h_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_h_j, *gcount_padded_j * sizeof(float), stream);
+  cudaMallocAsync(&d_mass_i, *gcount_padded_i * sizeof(float), stream);
+  cudaMallocAsync(&d_mass_j, *gcount_padded_j * sizeof(float), stream);
+  cudaMallocAsync(&d_x_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_x_j, *gcount_padded_j * sizeof(float), stream);
+  cudaMallocAsync(&d_y_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_y_j, *gcount_padded_j * sizeof(float), stream);
+  cudaMallocAsync(&d_z_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_z_j, *gcount_padded_j * sizeof(float), stream);
+  cudaMallocAsync(&d_a_x_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_a_y_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_a_z_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_a_x_j, *gcount_j * sizeof(float), stream);
+  cudaMallocAsync(&d_a_y_j, *gcount_j * sizeof(float), stream);
+  cudaMallocAsync(&d_a_z_j, *gcount_j * sizeof(float), stream);
+  cudaMallocAsync(&d_pot_i, *gcount_i * sizeof(float), stream);
+  cudaMallocAsync(&d_pot_j, *gcount_j * sizeof(float), stream);
+  cudaMallocAsync(&d_active_i, *gcount_i * sizeof(int), stream);
+  cudaMallocAsync(&d_mpole_i, *gcount_i * sizeof(int), stream);
+  cudaMallocAsync(&d_active_j, *gcount_j * sizeof(int), stream);
+  cudaMallocAsync(&d_mpole_j, *gcount_j * sizeof(int), stream);
+  cudaMallocAsync(&d_CoM_i, 3 * sizeof(float), stream);
+  cudaMallocAsync(&d_CoM_j, 3 * sizeof(float), stream);
 
   // copy data to device
-  cudaMemcpyAsync(d_h_i, h_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+  cudaMemcpyAsync(d_h_i, h_i, *gcount_i * sizeof(float), cudaMemcpyHostToDevice,
+                  stream);
   cudaMemcpyAsync(d_h_j, h_j_arr, *gcount_padded_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_mass_i, mass_i_arr, *gcount_padded_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_mass_j, mass_j_arr, *gcount_padded_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
-  cudaMemcpyAsync(d_x_i, x_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
+  cudaMemcpyAsync(d_x_i, x_i, *gcount_i * sizeof(float), cudaMemcpyHostToDevice,
+                  stream);
   cudaMemcpyAsync(d_x_j, x_j_arr, *gcount_padded_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
-  cudaMemcpyAsync(d_y_i, y_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
+  cudaMemcpyAsync(d_y_i, y_i, *gcount_i * sizeof(float), cudaMemcpyHostToDevice,
+                  stream);
   cudaMemcpyAsync(d_y_j, y_j_arr, *gcount_padded_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
-  cudaMemcpyAsync(d_z_i, z_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
+  cudaMemcpyAsync(d_z_i, z_i, *gcount_i * sizeof(float), cudaMemcpyHostToDevice,
+                  stream);
   cudaMemcpyAsync(d_z_j, z_j_arr, *gcount_padded_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_a_x_i, a_x_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_a_y_i, a_y_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_a_z_i, a_z_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_a_x_j, a_x_j, *gcount_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_a_y_j, a_y_j, *gcount_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_a_z_j, a_z_j, *gcount_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_pot_i, pot_i, *gcount_i * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_pot_j, pot_j, *gcount_j * sizeof(float),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_active_i, active_i, *gcount_i * sizeof(int),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_mpole_i, mpole_i, *gcount_i * sizeof(int),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_active_j, active_j, *gcount_j * sizeof(int),
-                  cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
   cudaMemcpyAsync(d_mpole_j, mpole_j, *gcount_j * sizeof(int),
-                  cudaMemcpyHostToDevice);
-  cudaMemcpyAsync(d_CoM_i, CoM_i, 3 * sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpyAsync(d_CoM_j, CoM_j, 3 * sizeof(float), cudaMemcpyHostToDevice);
+                  cudaMemcpyHostToDevice, stream);
+  cudaMemcpyAsync(d_CoM_i, CoM_i, 3 * sizeof(float), cudaMemcpyHostToDevice,
+                  stream);
+  cudaMemcpyAsync(d_CoM_j, CoM_j, 3 * sizeof(float), cudaMemcpyHostToDevice,
+                  stream);
 
   // printf("%.16f %.16f\n", x_i[0], y_i[0]);
 
@@ -291,7 +347,8 @@ extern "C" void pp_offload(
   // cudaDeviceSynchronize();
 
   // call kernel function
-  pair_grav_pp<<<1024, 1024>>>(
+  pair_grav_pp<<<gpu_info->sms_multiple * gpu_info->nr_sm,
+                 gpu_info->threads_per_block, 0, stream>>>(
       periodic, d_CoM_i, d_CoM_j, rmax_i, rmax_j, min_trunc, d_active_i,
       d_mpole_i, d_active_j, d_mpole_j, dim[0], dim[1], dim[2], d_h_i, d_h_j,
       d_mass_i, d_mass_j, *r_s_inv, d_x_i, d_x_j, d_y_i, d_y_j, d_z_i, d_z_j,
@@ -307,27 +364,27 @@ extern "C" void pp_offload(
 
   // copy data from device
   cudaMemcpyAsync(&a_x_i_new, d_a_x_i, *gcount_i * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
   cudaMemcpyAsync(&a_y_i_new, d_a_y_i, *gcount_i * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
   cudaMemcpyAsync(&a_z_i_new, d_a_z_i, *gcount_i * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
   cudaMemcpyAsync(&pot_i_new, d_pot_i, *gcount_i * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
 
   cudaMemcpyAsync(&a_x_j_new, d_a_x_j, *gcount_j * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
   cudaMemcpyAsync(&a_y_j_new, d_a_y_j, *gcount_j * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
   cudaMemcpyAsync(&a_z_j_new, d_a_z_j, *gcount_j * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
   cudaMemcpyAsync(&pot_j_new, d_pot_j, *gcount_j * sizeof(float),
-                  cudaMemcpyDeviceToHost);
+                  cudaMemcpyDeviceToHost, stream);
 
   // printf("%.16f %.16f %.16f %.16f\n", a_x_i_new[0], a_y_i_new[0],
   // a_z_i_new[0], pot_i_new[0]);
 
-  cudaDeviceSynchronize();
+  cudaStreamSynchronize(stream);
 
   cudaError_t err3 = cudaGetLastError();
   if (err3 != cudaSuccess) printf("Error3: %s\n", cudaGetErrorString(err3));
@@ -357,32 +414,32 @@ extern "C" void pp_offload(
   // pot_new[0]);
 
   // free memory
-  cudaFree(d_h_i);
-  cudaFree(d_h_j);
-  cudaFree(d_mass_i);
-  cudaFree(d_mass_j);
-  cudaFree(d_x_i);
-  cudaFree(d_x_j);
-  cudaFree(d_y_i);
-  cudaFree(d_y_j);
-  cudaFree(d_z_i);
-  cudaFree(d_z_j);
-  cudaFree(d_a_x_i);
-  cudaFree(d_a_y_i);
-  cudaFree(d_a_z_i);
-  cudaFree(d_a_x_j);
-  cudaFree(d_a_y_j);
-  cudaFree(d_a_z_j);
-  cudaFree(d_pot_i);
-  cudaFree(d_pot_j);
-  cudaFree(d_active_i);
-  cudaFree(d_mpole_i);
-  cudaFree(d_active_j);
-  cudaFree(d_mpole_j);
-  cudaFree(d_CoM_i);
-  cudaFree(d_CoM_j);
-  cudaFree(d_multi_j);
-  cudaFree(d_multi_i);
+  cudaFreeAsync(d_h_i, stream);
+  cudaFreeAsync(d_h_j, stream);
+  cudaFreeAsync(d_mass_i, stream);
+  cudaFreeAsync(d_mass_j, stream);
+  cudaFreeAsync(d_x_i, stream);
+  cudaFreeAsync(d_x_j, stream);
+  cudaFreeAsync(d_y_i, stream);
+  cudaFreeAsync(d_y_j, stream);
+  cudaFreeAsync(d_z_i, stream);
+  cudaFreeAsync(d_z_j, stream);
+  cudaFreeAsync(d_a_x_i, stream);
+  cudaFreeAsync(d_a_y_i, stream);
+  cudaFreeAsync(d_a_z_i, stream);
+  cudaFreeAsync(d_a_x_j, stream);
+  cudaFreeAsync(d_a_y_j, stream);
+  cudaFreeAsync(d_a_z_j, stream);
+  cudaFreeAsync(d_pot_i, stream);
+  cudaFreeAsync(d_pot_j, stream);
+  cudaFreeAsync(d_active_i, stream);
+  cudaFreeAsync(d_mpole_i, stream);
+  cudaFreeAsync(d_active_j, stream);
+  cudaFreeAsync(d_mpole_j, stream);
+  cudaFreeAsync(d_CoM_i, stream);
+  cudaFreeAsync(d_CoM_j, stream);
+  cudaFreeAsync(d_multi_j, stream);
+  cudaFreeAsync(d_multi_i, stream);
 
   cudaError_t err4 = cudaGetLastError();
   if (err4 != cudaSuccess) printf("Error4: %s\n", cudaGetErrorString(err4));
