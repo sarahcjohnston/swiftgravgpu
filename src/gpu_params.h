a0cb5156f68c33fc38d39a4cb711a2cea846b1da
wjr21 <wjr21@sussex.ac.uk>
2024-11-28 14:05:13 +0000

    Typo

--- src/gpu_params.h
+++ src/gpu_params.h
@@ -37,6 +37,6 @@ struct gpu_info {
   int nr_streams;
 };
 
-sturct gpu_info *gpu_init_info(struct swift_params *params);
+struct gpu_info *gpu_init_info(struct swift_params *params);
 
 #endif  // GPU_PARAMS_H
